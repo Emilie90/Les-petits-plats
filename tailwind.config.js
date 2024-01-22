58ffefb1183690425456db5f6df23f8425cf0f50
Emilie90 <emilie.voiron@get-mail.eu>
2024-01-22 12:06:17 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      "gris-souris": "#EDEDED",
-      gris: "#C6C6C6",
-      white: "#ffffff",
-      jaune: "#FFD15B",
-      "gris-dark": "#7A7A7A",
-      zinc: "#1B1B1B",
+export const content = ["./**/*.{html,js}"];
+export const theme = {
+  colors: {
+    transparent: "transparent",
+    current: "currentColor",
+    "gris-souris": "#EDEDED",
+    gris: "#C6C6C6",
+    white: "#ffffff",
+    jaune: "#FFD15B",
+    "gris-dark": "#7A7A7A",
+    zinc: "#1B1B1B",
+  },
+  extend: {
+    backgroundImage: {
+      "header-image":
+        "url('/images/lampos-aritonang-24gR_9lCdes-unsplash1.png')",
     },
-    extend: {
-      backgroundImage: {
-        "header-image":
-          "url('/images/lampos-aritonang-24gR_9lCdes-unsplash1.png')",
-      },
-      fontFamily: {
-        anton: ["Anton", "sans-serif"],
-        manrope: ["Manrope", "sans-serif"],
-      },
+    fontFamily: {
+      anton: ["Anton", "sans-serif"],
+      manrope: ["Manrope", "sans-serif"],
     },
   },
-  plugins: [],
 };
+export const plugins = [];
